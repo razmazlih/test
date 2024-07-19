8bcef3ca6d9ba02551945a24aa10fdfce4d9a808
razmazlih <razmazlih@users.noreply.github.com>
2024-07-19 09:14:39 +0000

    deploy: 01d9bc76dfe59f62f0e45cfcf519f9ae3e5cabfb

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const secret = '123123123123123123';
-const tag = document.getElementById('secret');
-
-console.log(secret);
-tag.innerHTML = secret;
+document.addEventListener("DOMContentLoaded", function() {
+    const secret = '123123123123123123';
+    const tag = document.getElementById('secret');
+  
+    console.log('Secret value:', secret); // הדפסת הערך של הסוד
+    if (tag) {
+      console.log('Element found:', tag); // בדיקה אם האלמנט נמצא
+      tag.innerHTML = secret;
+    } else {
+      console.error('Element with id "secret" not found.');
+    }
+  });
